295f8d907ea325904d2e61bb515d37eb5f4129b6
davidbatetc <davidbatetc+github@gmail.com>
2020-12-26 22:13:40 +0100

    More documentation added.

--- bot/cl/PolyVisitor.py
+++ bot/cl/PolyVisitor.py
@@ -7,27 +7,24 @@ else:
 
 # This class defines a complete generic visitor for a parse tree produced by PolyParser.
 
+
 class PolyVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by PolyParser#root.
-    def visitRoot(self, ctx:PolyParser.RootContext):
+    def visitRoot(self, ctx: PolyParser.RootContext):
         return self.visitChildren(ctx)
-
 
     # Visit a parse tree produced by PolyParser#stmt.
-    def visitStmt(self, ctx:PolyParser.StmtContext):
+    def visitStmt(self, ctx: PolyParser.StmtContext):
         return self.visitChildren(ctx)
-
 
     # Visit a parse tree produced by PolyParser#poly.
-    def visitPoly(self, ctx:PolyParser.PolyContext):
+    def visitPoly(self, ctx: PolyParser.PolyContext):
         return self.visitChildren(ctx)
-
 
     # Visit a parse tree produced by PolyParser#color.
-    def visitColor(self, ctx:PolyParser.ColorContext):
+    def visitColor(self, ctx: PolyParser.ColorContext):
         return self.visitChildren(ctx)
-
 
 
 del PolyParser
